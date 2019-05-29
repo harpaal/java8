1c04fd22bb150e84e7d5817ece70bd55d4c03bcf
Usr <Usr@DESKTOP-LQU7DOL>
2019-05-30 00:29:45 +0530

    updated with lombock

---
+++ src/main/java/com/hpst/java8/MenuUtil.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.hpst.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Usr
+ *
+ */
+public class MenuUtil {
+
+	public static List<Dish> getMenuList(){
+		return Arrays.asList(
+				new Dish("pork", false, 800, Dish.Type.MEAT),
+				new Dish("beef", false, 700, Dish.Type.MEAT),
+				new Dish("chicken", false, 400, Dish.Type.MEAT),
+				new Dish("french fries", true, 530, Dish.Type.OTHER),
+				new Dish("rice", true, 350, Dish.Type.OTHER),
+				new Dish("season fruit", true, 120, Dish.Type.OTHER),
+				new Dish("pizza", true, 550, Dish.Type.OTHER),
+				new Dish("prawns", false, 300, Dish.Type.FISH),
+				new Dish("salmon", false, 450, Dish.Type.FISH) );
+
+	}
+}
